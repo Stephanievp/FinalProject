4a88427039a0681a3fcbe41f1a1bec1406af37aa
Braulio <braumanhattan@gmail.com>
2020-04-30 17:18:48 -0400

    final project

---
+++ src/main/java/com/example/covidconnect/Detail_Info.java
@@ -0,0 +1,16 @@
+package com.example.covidconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Detail_Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail__info);
+    }
+
+
+}
